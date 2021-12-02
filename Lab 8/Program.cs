51f7a484610f44963b55e15dfdc021e3e9245547
Anton S <geodesist.sushkov@gmail.com>
2021-12-03 02:00:28 +0300

    Добавьте файлы проекта.

---
+++ Lab 8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = "C:/Users/User/source/repos";
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine(path);
+                string[] file = Directory.GetFiles(path);
+                foreach (string f in file)
+                {
+                    Console.WriteLine(f);
+                }
+                string[] dir = Directory.GetDirectories(path);
+                foreach (string subDir in dir)
+                {
+                    Console.WriteLine(subDir);
+                    string[] file2 = Directory.GetFiles(subDir);
+                    foreach (string f in file2)
+                    {
+                        Console.WriteLine(f);
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("каталог не найден");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 8.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Random.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int r = rnd.Next(100);
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.Write(r + " ");
+                    sw.Close();
+                }
+            }
+            int Sum = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string newString = sr.ReadToEnd();
+                string[] stringArray = newString.Split();
+                for (int i = 0; i < 10; i++)
+                {
+                    Sum += Convert.ToInt32(stringArray[i]);
+                }
+                Console.WriteLine(Sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
